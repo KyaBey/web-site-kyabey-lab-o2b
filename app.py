be5a7b0f4873a6bd3200b3a20426dbcad93a198d
KyaBey <karolynebardemaker@gmail.com>
2024-01-28 08:29:07 -0300

    Adição de .gitignore e modifição para variável de ambiente

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 from googleapiclient.discovery import build
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Carrega as variáveis de ambiente do arquivo .env
+
+# Acesse as variáveis de ambiente
+youtube_api_key = os.getenv("YOUTUBE_API_KEY")
 
 app = Flask(__name__)
 
 # Configure a sua chave de API do YouTube
-YOUTUBE_API_KEY = 'AIzaSyCoejfVc6ww4VtPK2wPECOzNRtd2yLDkrM'
-youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+youtube = build('youtube', 'v3', developerKey=youtube_api_key)
 
 def get_videos_from_playlist(playlist_id):
     playlist_items = youtube.playlistItems().list(part='snippet', playlistId=playlist_id, maxResults=50).execute()
